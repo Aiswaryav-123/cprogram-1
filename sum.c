03c3e361098a3204c0efbf65d5e14e94845e1313
Aiswaryav-123 <82395286+Aiswaryav-123@users.noreply.github.com>
2021-04-25 10:59:36 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+  int a,b,s;
+  printf("\n****************\n");
+  printf("Enter the first number:");
+  scanf("%d",&a);
+  printf("Enter the second number:");
+  scanf("%d",&b);
+  s=a+b;
+  printf("%d is the sum of %d and %d\n",s,a,b);
+  printf("****************\n ");
+}
