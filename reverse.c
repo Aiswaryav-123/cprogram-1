e5731ea3a0ecba0a90af3102e9043502976f0895
Aiswaryav-123 <82395286+Aiswaryav-123@users.noreply.github.com>
2021-04-28 13:28:50 +0530

    Add files via upload

---
+++ reverse.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+  int n,l,rev=0;
+  printf("Enter the number:");
+  scanf("%d",&n);
+  while(n>0)
+  {
+    l=n%10;
+    rev=rev*10+l;
+    n=n/10;
+
+  }
+  printf("reverse = %d\n",rev);
+
+}
