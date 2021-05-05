7ed1386dc563914000b40f2549220c533a2f063d
Aiswaryav-123 <82395286+Aiswaryav-123@users.noreply.github.com>
2021-05-05 12:36:35 +0530

    Add files via upload

---
+++ string1.c
@@ -0,0 +1,12 @@
+// TO PRINT A WORD
+#include<stdio.h>
+int main()
+{
+    char a[10];
+    printf("What is your name?");
+    scanf("%s",a);
+    printf("Welcome ");
+    printf("%s",a);
+    printf("\n");
+
+}
